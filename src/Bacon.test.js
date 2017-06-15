b31e13a937754573873418116a247f70215d6468
Mohamed Hamed <mohamed.hamed1@vodafone.com>
2017-06-16 01:20:40 +1200

    init commit

---
+++ src/Bacon.test.js
@@ -0,0 +1,24 @@
+      function nonEmpty(x) { return x.length > 0 }
+      function setVisibility(element, visible) {
+        element.toggle(visible)
+      }
+      function setEnabled(element, enabled) {
+        element.attr("disabled", !enabled)
+      }
+      
+      $(function() {
+        function textFieldVaule(textField){
+          function vaule(){return textField.val()}
+          return textField.asEventStream("keyup").map(value).toProperty(value())
+        }
+          $(".ajax").hide()
+          usernameField = $("#username input")
+          fullnameField = $("#fullname input")
+          registerButton = $("#register button")
+          unavailabilityLabel = $("#username-unavailable")
+          usernameAjaxIndicator = $("#username .ajax")
+          registerAjaxIndicator = $("#register .ajax")
+          username = Bacon.UI.textFieldValue($("#username input"))
+          fullname = Bacon.UI.textFieldValue($("#fullname input"))
+          username.log();
+      })
