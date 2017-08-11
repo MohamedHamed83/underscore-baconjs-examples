786b943b4c087686a12cfd0a9bce4c61d980b82f
Mohamed Hamed <mohamed.hamed1@vodafone.com>
2017-08-11 15:43:49 +1200

    Adding Baconjs Example

--- src/Bacon.spreadsheet.js
+++ src/Bacon.spreadsheet.js
@@ -1,5 +1,17 @@
-// Bacon.once(value) creates an EventStream that delivers the given single value for the first subscriber. 
-// var s = Bacon.once("pow");
+function fieldValue($field) {
+  function currentValue() {
+    return $field.val()
+  }
+  var changeE = $($field).asEventStream("keyup")
+  return changeE.map(currentValue).toProperty(currentValue())
+}
 
-//Bacon.sequentially(interval, values) creates a stream containing given values 
-// var s= Bacon.sequentially(200,[5,4,3,1,2, "pow"])
+var a = fieldValue($("#a")).map(parseInt);
+var b = fieldValue($("#b")).map(parseInt);
+var c = a.combine(b, function (aval, bval) {
+  return aval + bval;
+})
+
+c.onValue(function(newValue){
+  $("#c").val(newValue);
+})
